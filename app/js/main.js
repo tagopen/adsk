28f310780a4459e3134522fd712bd69b32668a38
Security2431 <Artem2431@gmail.com>
2017-08-01 21:42:38 +0300

    set date script

--- app/js/main.js
+++ app/js/main.js
@@ -64,6 +64,54 @@
     // Options will go here
   });
 
+  // Today date
+  $(function () {
+    Date.prototype.addDays = function(days) {
+      this.setDate(this.getDate() + parseInt(days));
+      return this;
+    };
+
+    Date.prototype.format = function(format) {
+      // set default format if function argument not provided
+      format = format || 'YYYY-MM-DD hh:mm';
+
+      var zeropad = function(number, length) {
+        number = number.toString();
+        length = length || 2;
+        while(number.length < length)
+          number = '0' + number;
+        return number;
+      },
+      // here you can define your formats
+      formats = {
+        YYYY: this.getFullYear(),
+        MM: zeropad(this.getMonth() + 1),
+        DD: zeropad(this.getDate()),
+        hh: zeropad(this.getHours()),
+        mm: zeropad(this.getMinutes())
+      },
+      pattern = '(' + Object.keys(formats).join(')|(') + ')';
+
+      return format.replace(new RegExp(pattern, 'g'), function(match) {
+        return formats[match];
+      });
+    };
+
+    var currentDate    = new Date(),
+        $dates         = $('[data-time]'),
+        $currentDate   = $('.date__number');
+
+    $currentDate.text(currentDate.format('DD.MM.YYYY'));
+
+    $dates.each(function() {
+      var $this = $(this),
+          addDate = $this.data('time');
+
+      currentDate.addDays(addDate);
+      $this.text(currentDate.format('DD.MM.YYYY'));
+    });
+  });
+
 
 
 })(jQuery); // End of use strict
